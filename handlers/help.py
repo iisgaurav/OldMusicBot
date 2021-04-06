38dd45f5a49dd247be8b6d9c17cbf9b72c682d65
Gaurav Verma <gauravv2504@gmail.com>
2021-04-06 18:31:50 +0530

    Update help.py

--- handlers/help.py
+++ handlers/help.py
@@ -1,15 +1,16 @@
 from pyrogram import Client, filters
+from pyrogram.types import Message
 
 
 
 @Client.on_message(
     filters.command("help")
+    & filters.private
+    & ~ filters.edited
 )
 async def help_(client: Client, message: Message):
     await message.reply_text(
-        f"""<b>Hi {message.from_user.first_name}!
-        
-    ❤ The commands and there use is explained here ❤
+        f"""❤ The commands and there use is explained here ❤
 - `/saavn` To search song on jio saavan and play the first result 
 - `/ytt` To search the song on Youtube and play the first matching result.
 - `/deezer` To search song on deezer and play good quality stream.
@@ -18,8 +19,22 @@ async def help_(client: Client, message: Message):
 - `/stop` to stop the streaming of song 
 - `/pause` to pause the stream 
 - `/resume` to resume the playback. 
-- Inline search is also supported."""
+- Inline search is also supported.""")
 
-
-
-    )
+@Client.on_message(
+    filters.command("help")
+    & filters.group
+    & ~ filters.edited
+)
+async def help(client: Client, message: Message):
+    await message.reply_text(
+        f"""❤ The commands and there use is explained here ❤
+- `/saavn` To search song on jio saavan and play the first result 
+- `/ytt` To search the song on Youtube and play the first matching result.
+- `/deezer` To search song on deezer and play good quality stream.
+- `/play` Reply this in response to a link or any telegram audio file it will be played 
+- `/skip` to skip current song 
+- `/stop` to stop the streaming of song 
+- `/pause` to pause the stream 
+- `/resume` to resume the playback. 
+- Inline search is also supported.""")
