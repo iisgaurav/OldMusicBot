099fc093fe2c5b1afb4e6ba5215b44970b84951d
sddgr807 <66588489+sddgr807@users.noreply.github.com>
2021-05-10 16:55:38 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -88,7 +88,7 @@ async def play(client: Client, message_: Message):
         reply_markup=InlineKeyboardMarkup(
             [[InlineKeyboardButton("Support⚡️", url="t.me/ThanosAssistant_Bot")]]
         ),
-        caption=f"Playing Your song Via  [Yogi Thanos Baba Bots Network.",
+        caption=f"Playing Your song Via  [Yogi Thanos Baba Bots Network]",
          ) 
         tgcalls.pytgcalls.join_group_call(message_.chat.id, file_path)
 
@@ -103,7 +103,7 @@ async def deezer(client: Client, message_: Message):
     requested_by = message_.from_user.first_name
     text = message_.text.split(" ", 1)
     queryy = text[1]
-    res = await message_.reply_text(f"Ruko beta, apka ganna dhund rahe hain deezer pe `{queryy}` ")
+    res = await message_.reply_text(f"Beta ruko jara sabar karo, apka ganna dhund rahe hain deezer pe `{queryy}` ")
     try:
         arq = ARQ("https://thearq.tech")
         r = await arq.deezer(query=queryy, limit=1)
@@ -124,7 +124,7 @@ async def deezer(client: Client, message_: Message):
     if message_.chat.id in tgcalls.pytgcalls.active_calls:
         await res.edit("adding in queue")
         position = sira.add(message_.chat.id, file_path)
-        await res.edit_text(f"Beta ruko jara sabar karo apka number hai {position}.")
+        await res.edit_text(f"Koi dhakka mukki nahi karega, apka number hai {position}.")
     else:
         await res.edit_text("Ok sur, apka ganna baja rahe hain aab")
         tgcalls.pytgcalls.join_group_call(message_.chat.id, file_path)
@@ -135,7 +135,7 @@ async def deezer(client: Client, message_: Message):
         reply_markup=InlineKeyboardMarkup(
             [[InlineKeyboardButton("Support⚡️", url="t.me/ThanosAssistant_Bot")]]
         ),
-        caption=f"Playing [{title}]({url}) Via [Deezer]."
+        caption=f"Playing [{title}]({url}) Via [Yogi Thanos Baba Bots Network]."
     ) 
     os.remove("final.png")
 # -----------------------------------------------------Jiosaavn-----------------------------------------------------------------
@@ -149,7 +149,7 @@ async def jiosaavn(client: Client, message_: Message):
     chat_id=message_.chat.id
     text = message_.text.split(" ", 1)
     query = text[1]
-    res = await message_.reply_text(f"Ruko beta, apka ganna dhund rahe hain jio saavn pe `{query}` ")
+    res = await message_.reply_text(f"Beta ruko jara sabar karo, apka ganna dhund rahe hain deezer pe jio saavn pe `{query}` ")
     try:
         async with aiohttp.ClientSession() as session:
             async with session.get(
@@ -171,7 +171,7 @@ async def jiosaavn(client: Client, message_: Message):
     file_path= await convert(wget.download(slink))
     if message_.chat.id in tgcalls.pytgcalls.active_calls:
         position = sira.add(message_.chat.id, file_path)
-        await res.edit_text(f"Beta ruko jara sabar karo apka number hai {position}.")
+        await res.edit_text(f"Koi dhakka mukki nahi karega, apka number hai {position}.")
     else:
         await res.edit_text("Ok sur, apka ganna baja rahe hain aab")
         tgcalls.pytgcalls.join_group_call(message_.chat.id, file_path)
@@ -208,7 +208,7 @@ async def ytp(client: Client, message_: Message):
     chat_id=message_.chat.id
     text = message_.text.split(" ", 1)
     query = text[1]
-    res = await message_.reply_text(f"Ruko beta, apka ganna dhund rahe hain youtube pe `{query}` ")
+    res = await message_.reply_text(f"Beta ruko jara sabar karo, apka ganna dhund rahe hain youtube pe `{query}` ")
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
         link = f"https://youtube.com{results[0]['url_suffix']}"
@@ -226,7 +226,7 @@ async def ytp(client: Client, message_: Message):
     file_path = await convert(download(link))
     if message_.chat.id in tgcalls.pytgcalls.active_calls:
         position = sira.add(message_.chat.id, file_path)
-        await res.edit_text(f"Beta ruko jara sabar karo apka number hai {position}.")
+        await res.edit_text(f"Koi dhakka mukki nahi karega, apka number hai {position}.")
     else:
         await res.edit_text("Ok sur, apka ganna baja rahe hain aab")
         tgcalls.pytgcalls.join_group_call(message_.chat.id, file_path)
@@ -235,7 +235,7 @@ async def ytp(client: Client, message_: Message):
     res.delete
     m = await client.send_photo(
         chat_id=message_.chat.id,
-        caption=f"Playing `{query}` Via [YouTube]",
+        caption=f"Playing `{query}` Via [Yogi Thanos Baba Bots Network]",
         photo="final.png",
         reply_markup=InlineKeyboardMarkup(
             [[InlineKeyboardButton("Watch on youtube", url=link)]]
