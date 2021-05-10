f7deea1574c73b877e5a874655efc51f3d425f10
sddgr807 <66588489+sddgr807@users.noreply.github.com>
2021-05-10 19:13:36 +0530

    Update help.py

--- handlers/help.py
+++ handlers/help.py
@@ -12,7 +12,7 @@ async def help_(client: Client, message: Message):
     await message.reply_text(
         f"""❤ The commands and there use is explained here ❤
 - `/saavn` To search song on jio saavan and play the first result 
-- `/ytt` To search the song on Youtube and play the first matching result.
+- `/play` To search the song on Youtube and play the first matching result.
 - `/deezer` To search song on deezer and play good quality stream.
 - `/song` To download a song from youtube.
 - `/play` Reply this in response to a link or any telegram audio file it will be played 
@@ -31,7 +31,7 @@ async def help(client: Client, message: Message):
     await message.reply_text(
         f"""❤ The commands and there use is explained here ❤
 - `/saavn` To search song on jio saavan and play the first result 
-- `/ytt` To search the song on Youtube and play the first matching result.
+- `/play` To search the song on Youtube and play the first matching result.
 - `/deezer` To search song on deezer and play good quality stream.
 - `/song` To download a song from youtube.
 - `/play` Reply this in response to a link or any telegram audio file it will be played 
